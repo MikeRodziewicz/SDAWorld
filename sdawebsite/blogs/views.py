ff1d751e2aefd4e99466a607145edc3aef79fe77
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-09-30 18:34:54 +0200

    working on bootstrap

--- sdawebsite/blogs/views.py
+++ sdawebsite/blogs/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("hello world")
-# Create your views here.
+posts = [
+    {
+        'author': 'Mike',
+        'title': 'First post',
+        'content': 'bla bla first',
+        'date_posted': 'October 2020'
+    },
+    {
+        'author': 'Ela',
+        'title': 'Second post',
+        'content': 'bla bla second',
+        'date_posted': 'September 2020'
+    }
+
+
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blogs/home.html', context)
+
+
+def about(request):
+    return render(request, 'blogs/about.html')
--- sdawebsite/sdawebsite/settings.py
+++ sdawebsite/sdawebsite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogs',
-    'courses'
+    'courses',
+    'blogs'
 
 ]
 
