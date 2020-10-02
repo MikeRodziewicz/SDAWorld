29761215923c008292a190d2e087818d6692237a
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-10-02 22:22:01 +0200

    added models to admin, added content,

--- sdawebsite/blogs/views.py
+++ sdawebsite/blogs/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Mike',
-        'title': 'First post',
-        'content': 'bla bla first',
-        'date_posted': 'October 2020'
-    },
-    {
-        'author': 'Ela',
-        'title': 'Second post',
-        'content': 'bla bla second',
-        'date_posted': 'September 2020'
-    }
-
-
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blogs/home.html', context)
 
