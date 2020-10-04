4435dbb901917047cbd45a10879c9e68454f1b2a
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-10-04 18:46:46 +0200

    added users app for reqistration

--- sdawebsite/sdawebsite/settings.py
+++ sdawebsite/sdawebsite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from blogs.apps import BlogsConfig
+from users.apps import AppConfig
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
-    'blogs'
+    'blogs',
+    'users'
 
 ]
 
