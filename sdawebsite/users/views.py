cbc7b5b323e52032822755d39c4c69a3a4db5698
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-10-09 09:52:23 +0200

    fixed lack of data in the form

--- sdawebsite/users/views.py
+++ sdawebsite/users/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created')
+            messages.success(request, 'Your account has been created')
             return redirect('login')
     else:
         form = UserRegister()
@@ -20,13 +20,18 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == 'POST':
-        p_form = ProfileUpdateForm(request.POST, instance=request.user)
-        u_form = UserUpdateForm(request.POST,
-                                request.FILES,
-                                instance=request.user.profile)
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
     else:
-        p_form = ProfileUpdateForm(instance=request.user)
-        u_form = UserUpdateForm(instance=request.user.profile)
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
