8aabccdbe240fd397dbccb989c6419b2a1170f60
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-10-10 19:51:55 +0200

    CRUD done - comments now working

--- sdawebsite/blogs/views.py
+++ sdawebsite/blogs/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    DeleteView
+    DeleteView,
+    UpdateView
 )
 from .models import Post, Comment
 
@@ -21,14 +23,14 @@ class PostListView(ListView):
     template_name = 'blogs/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    # paginate_by = 5
+    paginate_by = 10
 
 
 class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 # this is actually complicated - need further explanation
@@ -38,5 +40,32 @@ class PostCreateView(CreateView):
         return super().form_valid(form)
 
 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+# this is actually complicated - need further explanation
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 def about(request):
     return render(request, 'blogs/about.html')
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
--- sdawebsite/sdawebsite/urls.py
+++ sdawebsite/sdawebsite/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
